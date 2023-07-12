a788646c20f789fa3acd0161d86e6681b7573894
vishal arora <35168406+HASHtechvishal@users.noreply.github.com>
2023-07-12 23:29:48 +0530

    Add files via upload

---
+++ string_split.py
@@ -0,0 +1,21 @@
+#In Python, a string can be split on a delimiter.
+
+
+def split_and_join(line):
+
+    # write your code here
+
+    a = line.split(" ")
+
+    b = "-".join(a)
+
+    return(b)
+
+
+if __name__ == '__main__':
+
+    line = input()
+
+    result = split_and_join(line)
+
+    print(result)
